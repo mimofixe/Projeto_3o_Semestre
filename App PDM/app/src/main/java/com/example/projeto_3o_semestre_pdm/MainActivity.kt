b61e24c7e8a6c8dde30042863fcc27ef165b4ed9
mimofixe <150029262+mimofixe@users.noreply.github.com>
2025-09-24 17:58:26 +0100

    files for the project

---
+++ App PDM/app/src/main/java/com/example/projeto_3o_semestre_pdm/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.projeto_3o_semestre_pdm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projeto_3o_semestre_pdm.ui.theme.Projeto_3o_Semestre_PDMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Projeto_3o_Semestre_PDMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Projeto_3o_Semestre_PDMTheme {
+        Greeting("Android")
+    }
+}
---
+++ Documentacao/2ª Entrega/README_2ª_Entrega.md
@@ -0,0 +1,2 @@
+# Projeto_3o_Semestre
+Projeto de 3º semestre IADE 
